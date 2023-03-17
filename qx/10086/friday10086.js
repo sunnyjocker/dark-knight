27b0f3bf02244f3370b3444206c4c29a7058481e
mos <mos@cool.com>
2023-03-17 10:24:10 +0800

    update

--- qx/10086/friday10086.js
+++ qx/10086/friday10086.js
@@ -5,10 +5,14 @@ bodyContent = '// hacked by sunny\n';
 
 lines = body.split('\n')
 for (l in lines) {  
-  bodyContent += (lines[l] + '\n')
   if ("if(handle_type1 != 'recent'){" == lines[l].trim()) {
+    bodyContent += (lines[l] + '\n')
     bodyContent += "        // hacked by sunny\n"
     bodyContent += "        $(\".agreement-icon\").addClass('on')\n"
+  } else if ("showModal(\"modalnotic\")" == lines[l].trim()) {
+    bodyContent += "    doHandle(goodscode, goodsname,handle_type,is_shiwu,price)"
+  } else {
+    bodyContent += (lines[l] + '\n')
   }
 }
 
